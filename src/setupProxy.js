24482256de2d8914883fe419ad4a925619d44d6c
Git-Askhat <askat.shailoobekovich@mail.ru>
2022-03-30 03:07:35 +0600

    created proxy file

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware(['/data', , '/otherApi'], {
+    createProxyMiddleware(['/data'], {
       target: 'https://message-send-app.herokuapp.com',
     })
   );
