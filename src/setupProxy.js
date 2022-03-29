8d33c16e996139d0cb4a8525e65464155bf95d3b
Git-Askhat <askat.shailoobekovich@mail.ru>
2022-03-30 02:56:51 +0600

    .

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,9 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
-    app.use(
-      createProxyMiddleware(["/data", , "/otherApi"], { target: "https://message-send-app.herokuapp.com" })
-    );
-  };
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware(['/data', , '/otherApi'], {
+      target: 'https://message-send-app.herokuapp.com',
+    })
+  );
+};
