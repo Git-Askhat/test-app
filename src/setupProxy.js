6ce2980ec42ce1b339449799a87aa9258570fe24
Git-Askhat <askat.shailoobekovich@mail.ru>
2022-03-30 03:35:56 +0600

    deleted proxy

--- src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = function (app) {
-  app.use(
-    createProxyMiddleware(['/data'], {
-      target: 'https://message-send-app.herokuapp.com',
-    })
-  );
-};
